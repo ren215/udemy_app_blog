e48907f9514af8d140b164157ca38260dd8a56eb
ren <asupc38@gmail.com>
2025-05-18 20:55:54 +0900

    環境構築

---
+++ blog-app/src/app/layout.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from 'react';
+import './globals.css';
+
+type Props = {
+  children: ReactNode;
+};
+
+export default function RootLayout({ children }: Props) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
